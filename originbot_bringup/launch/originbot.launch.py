0170d6de3f954ea7a7b84464a596923c813460a8
Hu Chunxu <huchunxu@ps-micro.com>
2022-08-10 23:52:46 +0800

    优化launch文件对参数的配置

--- originbot_bringup/launch/originbot.launch.py
+++ originbot_bringup/launch/originbot.launch.py
@@ -7,13 +7,7 @@ from ament_index_python.packages import get_package_share_directory
 
 
 def generate_launch_description():
-    ld = launch.LaunchDescription([
-        launch.actions.DeclareLaunchArgument(name='port_name',
-                                             default_value='ttyS3'),
-        launch.actions.DeclareLaunchArgument(name='correct_factor_vx',
-                                             default_value='1.0'),
-        launch.actions.DeclareLaunchArgument(name='correct_factor_vth',
-                                             default_value='0.868'),                                                                                
+    ld = launch.LaunchDescription([                                                                               
         launch.actions.DeclareLaunchArgument(name='open_rviz',
                                              default_value='false'),
         launch_ros.actions.Node(
@@ -28,11 +22,7 @@ def generate_launch_description():
             launch.launch_description_sources.PythonLaunchDescriptionSource(
                 os.path.join(get_package_share_directory('originbot_base'),
                              'launch/robot.launch.py')),
-            launch_arguments={
-                'port_name': launch.substitutions.LaunchConfiguration('port_name'),
-                'correct_factor_vx': launch.substitutions.LaunchConfiguration('correct_factor_vx'),
-                'correct_factor_vth': launch.substitutions.LaunchConfiguration('correct_factor_vth'),
-            }.items())
+            )
     ])
     return ld
 
