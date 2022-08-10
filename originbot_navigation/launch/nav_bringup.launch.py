ce4e06c7e07ceeda75a3e54ba679fea44ecae332
Hu Chunxu <huchunxu@ps-micro.com>
2022-08-10 21:34:45 +0800

    在导航中添加一个固定坐标系

--- originbot_navigation/launch/nav_bringup.launch.py
+++ originbot_navigation/launch/nav_bringup.launch.py
@@ -17,10 +17,20 @@ def generate_launch_description():
     map_yaml_path = LaunchConfiguration('map',default=os.path.join(originbot_navigation_dir,'maps','my_map.yaml'))
     nav2_param_path = LaunchConfiguration('params_file',default=os.path.join(originbot_navigation_dir,'param','originbot_nav2.yaml'))
 
+
+    originbot_imu_tf = Node(
+        package='tf2_ros',
+        executable='static_transform_publisher', 
+        emulate_tty=True,
+        arguments="0.0 0.0 0.0 0.0 0.0 0.0 /map /odom".split(
+                ' '))
+
     return LaunchDescription([
         DeclareLaunchArgument('use_sim_time',default_value=use_sim_time,description='Use simulation (Gazebo) clock if true'),
         DeclareLaunchArgument('map',default_value=map_yaml_path,description='Full path to map file to load'),
         DeclareLaunchArgument('params_file',default_value=nav2_param_path,description='Full path to param file to load'),
+
+        originbot_imu_tf,
 
         IncludeLaunchDescription(
             PythonLaunchDescriptionSource([nav2_bringup_dir,'/launch','/bringup_launch.py']),
