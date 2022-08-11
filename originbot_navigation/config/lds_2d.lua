d51ff66fb004cefa29735c1bb7d5bbe8b32afeb2
Hu Chunxu <huchunxu@ps-micro.com>
2022-08-11 20:01:03 +0800

    调试cartographer参数

--- originbot_navigation/config/lds_2d.lua
+++ originbot_navigation/config/lds_2d.lua
@@ -25,7 +25,7 @@ options = {
   published_frame = "odom",
   odom_frame = "odom",
   provide_odom_frame = false,
-  publish_frame_projected_to_2d = true,
+  publish_frame_projected_to_2d = false,
   use_odometry = true,
   use_nav_sat = false,
   use_landmarks = false,
@@ -47,13 +47,13 @@ options = {
 MAP_BUILDER.use_trajectory_builder_2d = true
 
 TRAJECTORY_BUILDER_2D.min_range = 0.1
-TRAJECTORY_BUILDER_2D.max_range = 4
-TRAJECTORY_BUILDER_2D.missing_data_ray_length = 2
+TRAJECTORY_BUILDER_2D.max_range = 6
+TRAJECTORY_BUILDER_2D.missing_data_ray_length = 0.1
 TRAJECTORY_BUILDER_2D.use_imu_data = false
 TRAJECTORY_BUILDER_2D.use_online_correlative_scan_matching = true 
 TRAJECTORY_BUILDER_2D.motion_filter.max_angle_radians = math.rad(0.1)
 
-POSE_GRAPH.constraint_builder.min_score = 0.6
+POSE_GRAPH.constraint_builder.min_score = 0.7
 POSE_GRAPH.constraint_builder.global_localization_min_score = 0.7
 
 --POSE_GRAPH.optimize_every_n_nodes = 30
