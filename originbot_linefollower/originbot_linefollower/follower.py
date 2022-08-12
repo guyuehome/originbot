9694afdfb5ac079d08f39357f99be0964165e703
Hu Chunxu <huchunxu@ps-micro.com>
2022-08-13 01:19:23 +0800

    添加OpenCV视觉巡线功能包，待实物调试

---
+++ originbot_linefollower/originbot_linefollower/follower.py
@@ -0,0 +1,58 @@
+import rclpy, cv2, cv_bridge, numpy
+from rclpy.node import Node
+from sensor_msgs.msg import Image
+from geometry_msgs.msg import Twist
+
+class Follower(Node):
+  def __init__(self):
+        super().__init__('line_follower')
+
+        # cv2.namedWindow("window", 1)
+
+        self.bridge = cv_bridge.CvBridge()
+        
+        self.image_sub = self.create_subscription(Image, '/image_raw', self.image_callback, 10)
+        self.cmd_vel_pub = self.create_publisher(Twist, 'cmd_vel', 10)
+        self.pub = self.create_publisher(Image, '/camera/process_image', 10)
+
+        self.twist = Twist()
+
+  def image_callback(self, msg):
+    image = self.bridge.imgmsg_to_cv2(msg, 'bgr8')
+    hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    lower_yellow = numpy.array([ 10,  10,  10])
+    upper_yellow = numpy.array([255, 255, 250])
+    mask = cv2.inRange(hsv, lower_yellow, upper_yellow)
+    
+    h, w, d = image.shape
+    search_top = 3*h/4
+    search_bot = 3*h/4 + 20
+    mask[0:search_top, 0:w] = 0
+    mask[search_bot:h, 0:w] = 0
+    M = cv2.moments(mask)
+
+    if M['m00'] > 0:
+      cx = int(M['m10']/M['m00'])
+      cy = int(M['m01']/M['m00'])
+      cv2.circle(image, (cx, cy), 20, (0,0,255), -1)
+
+      # BEGIN CONTROL
+      err = cx - w/2
+      self.twist.linear.x = 0.1
+      self.twist.angular.z = -float(err) / 100
+      self.cmd_vel_pub.publish(self.twist)
+      # END CONTROL
+
+    # cv2.imshow("window", image)
+    # cv2.waitKey(3)
+    self.pub.publish(self.bridge.cv2_to_imgmsg(image, 'bgr8'))
+
+def main(args=None):
+    rclpy.init(args=args)
+    follower = Follower()
+    rclpy.spin(follower)
+    follower.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
