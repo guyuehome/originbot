9772ac1fbedbcd0e564c3e02e8ab70ec52480e28
huchunxu <huchunxu@ps-micro.com>
2022-09-23 12:31:00 +0800

    补充各功能包的说明

--- originbot_linefollower/setup.py
+++ originbot_linefollower/setup.py
@@ -17,10 +17,10 @@ setup(
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='hcx',
-    maintainer_email='huchunxu@guyuehome.com',
-    description='TODO: Package description',
-    license='TODO: License declaration',
+    maintainer='GuYueHome',
+    maintainer_email='support@guyuehome.com',
+    description='originbot linefollower',
+    license='Apache 2.0',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
