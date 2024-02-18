7b61741393250025d81b0e7b066bc2c5c872eefb
yzhcat <yzhcat@qq.com>
2024-02-18 16:44:42 +0800

    添加 Image 和 CompressedImage 两种话题格式

--- originbot_example/utils/src/image_transport_node.cpp
+++ originbot_example/utils/src/image_transport_node.cpp
@@ -12,22 +12,21 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "cv_bridge/cv_bridge.h"
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/header.hpp"
-
-#include "opencv2/opencv.hpp"
+#include "cv_bridge/cv_bridge.h"
 #include "sensor_msgs/msg/compressed_image.hpp"
 
 #include "hbm_img_msgs/msg/hbm_msg1080_p.hpp"
 
 using hbm_img_msgs::msg::HbmMsg1080P;
 using sensor_msgs::msg::CompressedImage;
+using sensor_msgs::msg::Image;
 using std::placeholders::_1;
 
 class Nv122BGR : public rclcpp::Node
 {
-  public:
+public:
     Nv122BGR(std::string node_name = "Nv122BGR") : Node(node_name)
     {
         RCLCPP_INFO(this->get_logger(), node_name);
@@ -35,10 +34,11 @@ class Nv122BGR : public rclcpp::Node
         // 创建消息订阅者，从摄像头节点订阅图像消息
         sublisher_ = this->create_subscription_hbmem<HbmMsg1080P>(
             "/hbmem_img", 10, std::bind(&Nv122BGR::image_callback, this, std::placeholders::_1));
-        image_pub_ = this->create_publisher<sensor_msgs::msg::CompressedImage>("/image_out", 10);
+        compress_pub_ = this->create_publisher<CompressedImage>("/image_out/compressed", 10);
+        image_pub_ = this->create_publisher<Image>("/image_out", 10);
     }
 
-  private:
+private:
     void image_callback(const HbmMsg1080P::ConstSharedPtr img_msg)
     {
         // 对订阅到的图片消息进行验证，本示例只支持处理NV12格式图片数据
@@ -51,15 +51,15 @@ class Nv122BGR : public rclcpp::Node
         }
         cv::Mat nv12Image(img_msg->height * 3 / 2, img_msg->width, CV_8UC1,
                           const_cast<uint8_t *>(img_msg->data.data()));
-
         cv::Mat bgrImage;
         cv::cvtColor(nv12Image, bgrImage, cv::COLOR_YUV2BGR_NV12);
 
-        std_msgs::msg::Header hdr;
-        auto rosMsg = cv_bridge::CvImage(hdr, "bgr8", bgrImage).toCompressedImageMsg();
-        image_pub_->publish(std::move(*rosMsg));
+        auto cvImage = cv_bridge::CvImage(std_msgs::msg::Header(), "bgr8", bgrImage);
+        compress_pub_->publish(std::move(*cvImage.toCompressedImageMsg(cv_bridge::JPEG)));
+        image_pub_->publish(std::move(*cvImage.toImageMsg()));
     }
-    rclcpp::Publisher<sensor_msgs::msg::CompressedImage>::SharedPtr image_pub_;
+    rclcpp::Publisher<CompressedImage>::SharedPtr compress_pub_;
+    rclcpp::Publisher<Image>::SharedPtr image_pub_;
     rclcpp::SubscriptionHbmem<HbmMsg1080P>::ConstSharedPtr sublisher_ = nullptr;
 };
 
